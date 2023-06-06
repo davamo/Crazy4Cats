14b4cb9e44db909854b3cdd0716e8af344813696
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-06-06 07:49:21 -0400

    Feat: add  views posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :comments, dependent: :destroy
 
-  # Validaciones
   validates :title, presence: true
   validates :content, presence: true
+
+  def total_likes
+    likes - dislikes
+  end
+
 end
